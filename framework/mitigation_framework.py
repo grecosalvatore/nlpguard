f990735353bd21acef0d896867fedfda96f84850
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2023-12-06 10:28:28 +0000

    Initial Commit

--- framework/mitigation_framework.py
+++ framework/mitigation_framework.py
@@ -1,23 +1,64 @@
 import os
+import time
+import datetime
 
 
 class MitigationFramework:
     """ """
-    def __init__(self, use_case_name, output_folder=None):
-        # Init use case name
-        self.use_case_name = use_case_name
-        # Init output folder
-        if output_folder is not None:
-            self.output_folder = output_folder
+    def __init__(self):
+        self.use_case_name = None
+        self.output_folder = None
+        return
+
+
+    def initialize_mitigation_framework(self, use_case_name=None, output_folder=None):
+        self.output_folder, self.use_case_name = self._init_output_folders(use_case_name, output_folder)
+        return
+
+    def run_full_mitigation_pipeline(self, use_case_name=None, output_folder=None):
+        return
+
+    def run_explainer(self):
+        return
+
+    def run_identifier(self):
+        return
+
+    def run_moderator(self):
+        return
+
+    @staticmethod
+    def _init_output_folders(use_case_name, output_folder):
+        """ Initializes the output folders. """
+        # Init output folder. If output folder is not specified, the dafault is "outputs".
+        if output_folder is None:
+            output_folder = ("outputs")
+
+        # Create the output folder if it does not exist.
+        if not os.path.exists(output_folder):
+            print("Mitigation Framework: Warning, output folder does not exist.")
+            os.mkdir(output_folder)
+            print("Mitigation Framework: Output folder created.")
+
+        # Init use case folder. If use_case_name is not specified, the dafault is the current timestamp.
+        if use_case_name is None:
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d_%H%M%S')
+            use_case_name = timestamp
         else:
-            self.output_folder = os.path.join("..", "outputs")
+            if os.path.exists(os.path.join(output_folder, use_case_name)):
+                print("Mitigation Framework: Warning, specified use_case_name already exists in output folder.")
+                ts = time.time()
+                timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d_%H%M%S')
+                use_case_name = use_case_name + "_" + timestamp
 
-        self._init_outputs_folders(use_case_name, output_folder)
-        return
-
-    def _init_outputs_folders(self, use_case_name, output_folder):
-
-        return
+        use_case_folder_path = os.path.join(output_folder, use_case_name)
+        os.mkdir(use_case_folder_path)
+        os.mkdir(os.path.join(use_case_folder_path, "explainer_outputs"))
+        os.mkdir(os.path.join(use_case_folder_path, "identifier_outputs"))
+        os.mkdir(os.path.join(use_case_folder_path, "moderator_outputs"))
+        print(f"Mitigation Framework: Initialized output folder - {use_case_folder_path}.")
+        return output_folder, use_case_name
 
     def run_explainer(self, explainer_method="integrated-gradients"):
 
---
+++ run_mitigation.py
@@ -0,0 +1,5 @@
+from framework.mitigation_framework import MitigationFramework
+
+if __name__ == '__main__':
+    mf = MitigationFramework()
+    mf.initialize_mitigation_framework(use_case_name="bert_toxicity")
